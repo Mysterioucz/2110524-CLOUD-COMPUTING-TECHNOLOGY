e4dfe4ba81972f4de405ddd2431ebf08706b9611
Mysterioucz <chatrinyoonchalard@gmail.com>
2026-03-09 03:01:34 +0700

    fix: database

--- midterm/main.tf
+++ midterm/main.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block = "172.16.0.0/16"
+  enable_dns_support   = true   # Added for RDS/WP stability
+  enable_dns_hostnames = true   # Added for the nslookup requirement
   tags       = { Name = "VPC" }
 }
 
